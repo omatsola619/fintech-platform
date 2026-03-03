0bbe0ced246e6a5a565f37e327d0801f74dedc9c
omash <omarsunday0@gmail.com>
2026-03-03 13:12:56 +0100

    added tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { queryClient } from "./lib/react-query";
 import AuthLayout from "./pages/auth/AuthLayout";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import Login from "./pages/auth/Login";
@@ -19,37 +22,40 @@ import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<Dashboard />}>
-          <Route index element={<Overview />} />
-          <Route path="transactions" element={<Transactions />} />
-          <Route path="providers" element={<Providers />} />
-          <Route path="logs" element={<Logs />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route index element={<Overview />} />
+            <Route path="transactions" element={<Transactions />} />
+            <Route path="providers" element={<Providers />} />
+            <Route path="logs" element={<Logs />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
 
-        <Route path="/docs" element={<DocsPage />}>
-          <Route index element={<Introduction />} />
-          <Route
-            path="introduction"
-            element={<Navigate to="/docs" replace />}
-          />
-          <Route path="installation" element={<Installation />} />
-          <Route path="authentication" element={<Authentication />} />
-        </Route>
+          <Route path="/docs" element={<DocsPage />}>
+            <Route index element={<Introduction />} />
+            <Route
+              path="introduction"
+              element={<Navigate to="/docs" replace />}
+            />
+            <Route path="installation" element={<Installation />} />
+            <Route path="authentication" element={<Authentication />} />
+          </Route>
 
-        <Route path="/auth" element={<AuthLayout />}>
-          <Route index element={<Navigate to="login" replace />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="forgot-password" element={<ForgotPassword />} />
-          <Route path="verify" element={<VerifyCode />} />
-          <Route path="reset-password" element={<ResetPassword />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path="/auth" element={<AuthLayout />}>
+            <Route index element={<Navigate to="login" replace />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="forgot-password" element={<ForgotPassword />} />
+            <Route path="verify" element={<VerifyCode />} />
+            <Route path="reset-password" element={<ResetPassword />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
